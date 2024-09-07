7b113ea889a64afe7fecab1077ce26fa46a54ef6
Mudassar <mudassar0606@gmail.com>
2024-09-07 17:39:28 +0530

    Github OAuth

--- app/actions.ts
+++ app/actions.ts
@@ -128,3 +128,19 @@ export const signOutAction = async () => {
   await supabase.auth.signOut();
   return redirect("/sign-in");
 };
+
+export const signInWithGithub = async () =>{
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+    options: {
+      redirectTo: 'http://localhost:3000/api/auth/callback',
+    },
+  })
+
+  if (data.url) {
+    redirect(data.url) // use the redirect API for your server framework
+  }
+
+  return redirect("/protected");
+}
